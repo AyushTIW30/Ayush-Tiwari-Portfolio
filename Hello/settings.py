4fb229a5de6263cb88bb8e952c0cddfcf4c042a3
sactiwar <sactiwar@gmail.com>
2026-01-08 21:53:20 +0530

    Add PostgreSQL support

--- Hello/settings.py
+++ Hello/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -79,12 +80,14 @@ WSGI_APPLICATION = 'Hello.wsgi.application'
 
 
 # ================= DATABASE =================
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
+
 
 
 # ================= PASSWORD =================
